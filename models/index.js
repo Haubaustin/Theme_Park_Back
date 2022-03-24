f98e9bbe490461795fcff49365a981dd46385dfa
Mark Harmon <neothej1sec@gmail.com>
2022-03-24 09:40:28 -0500

    typo

--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 const ParkSchema = require('./park')
 const RideSchema = require('./ride')
-const PackageSchema = require('./packages')
+const PackageSchema = require('./package')
 
 const Park = mongoose.model('parks', ParkSchema)
 const Ride = mongoose.model('rides', RideSchema)
-const Package = mongoose.model('packages', RideSchema)
+const Package = mongoose.model('packages', PackageSchema)
 
 module.exports = {
   Park,
