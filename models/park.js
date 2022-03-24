b3cac4b95bcb020a42eebe9200f18ec875a97de7
Eloy Sauceda <eloysauceda4@outlook.com>
2022-03-23 20:24:26 -0500

    parksRides commit

--- models/park.js
+++ models/park.js
@@ -5,7 +5,7 @@ const Park = new Schema (
     name: { type: String, required: true },
     location: { type: String, required: true },
     url: { type: String, required: true },
-    avg_price: { type: Number, required: true },
+    avg_price: { type: String, required: true },
     image: { type: String, required: true }
 },
 { timestamps: true }
