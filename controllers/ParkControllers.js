6332e59958b4a7ee45e96ee09e3cda79932d030a
Mark Harmon <neothej1sec@gmail.com>
2022-03-24 13:37:07 -0500

    added app.listener

--- controllers/ParkControllers.js
+++ controllers/ParkControllers.js
@@ -1,18 +1,18 @@
-const { Park } = require("../models");
+// const { Park } = require("../models");
 
-const deletePark = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deleted = await Park.findByIdAndDelete(id)
-        if (deleted) {
-            return res.status(200).send("Park deleted");
-        }
-        throw new Error("Plant has been removed");
-    } catch (error) {
-        return res.status(500).send(error.message);
-    }
-}
+// const deletePark = async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         const deleted = await Park.findByIdAndDelete(id)
+//         if (deleted) {
+//             return res.status(200).send("Park deleted");
+//         }
+//         throw new Error("Plant has been removed");
+//     } catch (error) {
+//         return res.status(500).send(error.message);
+//     }
+// }
 
-module.exports = {
-    deletePark
-}
+// module.exports = {
+//     deletePark
+// }
--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express')
 const cors = require('cors')
 const PORT = process.env.PORT || 3001
 const db = require('./db')
-const routes = require('./routes');
+//const routes = require('./routes');
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use('/api', routes)
+//app.use('/api', routes)
 app.get('/', (req, res) => {
     res.send('This is our Project for themeparks!')
 })
@@ -45,7 +45,8 @@ app.get('/rides', async (req, res) => {
     const packages = await Package.find()
     res.json(packages)
   })
-  
+  db.on('error', console.error.bind(console, 'MongoDB connection error'))
+  app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
 
 
 
