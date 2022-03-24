b2f75d2160926d4068b8eda425d1ce53ecae1a52
Mark Harmon <neothej1sec@gmail.com>
2022-03-24 09:36:15 -0500

    added package model, appended package model to model/index, and added packages route in server

--- models/index.js
+++ models/index.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose')
 const ParkSchema = require('./park')
 const RideSchema = require('./ride')
+const PackageSchema = require('./packages')
 
 const Park = mongoose.model('parks', ParkSchema)
 const Ride = mongoose.model('rides', RideSchema)
+const Package = mongoose.model('packages', RideSchema)
 
 module.exports = {
   Park,
-  Ride
+  Ride,
+  Package
 }
